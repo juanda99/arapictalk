aa366869252ea835b30fed652a6919c5a0583631
juanda <juandacorreo@gmail.com>
2026-05-13 00:11:07 +0200

    Fix final lint errors in Header: remove unused imports

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,8 +10,8 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ZoomInIcon from '@mui/icons-material/ZoomIn';
 import ZoomOutIcon from '@mui/icons-material/ZoomOut';
 import AspectRatioIcon from '@mui/icons-material/AspectRatio';
-import { useAtom, useAtomValue } from 'jotai';
-import { isSidebarOpenAtom, zoomLevelAtom } from '../store/atoms/uiState';
+import { useAtom } from 'jotai';
+import { zoomLevelAtom } from '../store/atoms/uiState';
 
 export const Header: React.FC = () => {
   const [zoom, setZoom] = useAtom(zoomLevelAtom);
